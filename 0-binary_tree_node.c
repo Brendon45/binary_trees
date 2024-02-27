3de1c0dd387e97bca96ebf8a7ea73746d3c2ed41
Brendon45 <jejebrendon722@gmail.com>
2024-02-27 02:40:48 -0800

    a function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,11 +11,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *newnode;
+	binary_tree_t *new_node;
 
 	/* Allocate memory for the new node */
-	newnode = malloc(sizeof(binary_tree_t));
-	if (newnode == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
 		/*Memory allocation failed*/
 		return (NULL);
